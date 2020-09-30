1b48f59d74ae8fc582fe8aa51b6cc606f02e19a9
NCGill1997 <71213196+NCGill1997@users.noreply.github.com>
2020-09-30 11:21:06 -0400

    Added Car Generation, Car Prefab Editing
    Added the generation of the cars in lanes, as well as changed some of the prefabs to more efficiently spawn them so they are not floating or in the ground.
    
    Collisions did not work at last check.

---
+++ Assets/Scripts/NCG_Scripts/CarGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGenerator : MonoBehaviour
+{
+    public GameObject[] carPrefabs = new GameObject[4];
+    public GameObject[] lanes = new GameObject[3];
+
+    void Start()
+    {
+        StartCoroutine(LaneSpawning1());
+        StartCoroutine(LaneSpawning2());
+        StartCoroutine(LaneSpawning3());
+    }
+
+    #region IEnumerators
+    //Spawning rates are hard-coded for now.  These can be easily randomized, but for testing purposes it was easier to just use hard-coded
+    //times.  Each of these 
+
+    private IEnumerator LaneSpawning1()
+    {
+        int car = Random.Range(0, 4);
+
+        yield return new WaitForSeconds(3);
+        Instantiate(carPrefabs[car], lanes[0].transform.position, lanes[0].transform.rotation);
+
+        StartCoroutine(LaneSpawning1());
+    }
+
+    private IEnumerator LaneSpawning2()
+    {
+        int car = Random.Range(0, 4);
+        yield return new WaitForSeconds(2);
+        Instantiate(carPrefabs[car], lanes[1].transform.position, lanes[0].transform.rotation);
+
+        StartCoroutine(LaneSpawning2());
+    }
+
+    private IEnumerator LaneSpawning3()
+    {
+        int car = Random.Range(0, 4);
+        yield return new WaitForSeconds(1);
+        Instantiate(carPrefabs[car], lanes[2].transform.position, lanes[0].transform.rotation);
+
+        StartCoroutine(LaneSpawning3());
+    }
+    #endregion
+}
---
+++ Assets/Scripts/NCG_Scripts/CarScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+    private void Awake() //call for the destruction of the car in time.  This will be replaced with a kill box after a while.
+    {
+        StartCoroutine(DestroySelf());
+    }
+
+    private void Update() //Movement for the car.
+    {
+        transform.Translate(new Vector3(0, -Time.deltaTime * 3, 0));
+    }
+
+    private void OnCollisionEnter(Collision collision) //Collision setup to interact with the player
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player hit " + this.name);
+        }
+    }
+
+    IEnumerator DestroySelf()  //This was the "Plan B" for the destruction of the cars.
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("Destroy" + this.gameObject.name);
+        Destroy(gameObject);
+    }
+
+
+}
