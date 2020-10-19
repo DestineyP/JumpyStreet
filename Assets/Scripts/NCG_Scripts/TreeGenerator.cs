1a45f9e93d73b9c4db62caeac94b40abd8f723a2
NCGill1997 <71213196+NCGill1997@users.noreply.github.com>
2020-10-19 12:04:43 -0400

    Spawned trees, created prefabs, created spawnpoints
    I created the entire spawning setup for the trees, including waypoints, the code, the prefabs, and applying base colors to the tree meshes, as well as edited colliders on them to fit the trees.

---
+++ Assets/Scripts/NCG_Scripts/TreeGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGenerator : MonoBehaviour
+{
+    public GameObject[] trees = new GameObject[8];
+    public GameObject[] spawners = new GameObject[15];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTrees();
+    }
+
+    private void SpawnTrees()
+    {
+        for(int i = 0; i < 15; i++)
+        {
+            int spawnChance = Random.Range(1, 4); //creates a chance to spawn the tree
+            if(spawnChance < 3)
+            {
+                int randomTree = Random.Range(0, 8);//random tree from available meshes
+                Instantiate(trees[randomTree], spawners[i].transform.position, trees[randomTree].transform.rotation);
+            }
+        }
+    }
+}
