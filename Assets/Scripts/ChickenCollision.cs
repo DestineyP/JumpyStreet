fc1be3fee37612f2540912eecf51858386341c24
Destiney <dsprice1@my.waketech.edu>
2020-10-21 23:30:43 -0400

    Raycast tree colliders
    Raycast now causes collision effect with trees.

--- Assets/Scripts/ChickenCollision.cs
+++ Assets/Scripts/ChickenCollision.cs
@@ -60,6 +60,7 @@ public class ChickenCollision : MonoBehaviour
 
         }
 
+       
 
     }
 
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
 
-
+    public static MovementScript MoveScript;
     bool movedbackwards = false;
+    
+    bool moveForward = false;
+    
     public int moveBackCounter = 0;
     public Animator anim;
-
+   
     public  GameObject egg;
+    
     private float moveSpeed = 3f;
     private float gridSize = 1f;
     private enum Orientation
@@ -26,22 +31,28 @@ public class MovementScript : MonoBehaviour
     private float factor;
     private bool isMoving = false;
     public bool canMove;
-    bool isMovingForward;
+    
+
+    Vector3 sidemove;
 
     private void Start()
     {
-        isMovingForward = false;
-        anim.SetBool("isMovingForward", isMovingForward); 
+        MoveScript = this;
+        
+        
         canMove = true;
-        isMovingForward = false;
+   
+        
     }
     public void Update()
     {
+
+        
        
         if (!isMoving)
         {
             
-            MoveDirection = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+            MoveDirection = new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0);
             
                 if (Mathf.Abs(MoveDirection.x) > Mathf.Abs(MoveDirection.y))   ///making sure the player cannot move diagnal
                 {
@@ -51,31 +62,41 @@ public class MovementScript : MonoBehaviour
                 {
                     MoveDirection.x = 0;
                 }
-           
+            
 
-            if (MoveDirection != Vector2.zero && canMove) ///if the player is trying to move in a direction start the move corutine
+            if (MoveDirection != Vector2.zero) ///if the player is trying to move in a direction start the move corutine
             {
                 
                 StartCoroutine(moveTowardPosition(transform));
-                if (movedbackwards == false)
+                
+
+                if (moveForward == true)
                 {
-                    gameObject.transform.parent = null;
-                    anim.SetBool("isMovingForward", true);
+                    
+                    gameObject.transform.parent = null; // If attatched to a log, unattaching.
+                    Debug.Log("Forward");
+                    //transform.rotation = Quaternion.LookRotation(MoveDirection);
                     ScoreKeeper.ScoreScript.AddScore();
                     moveBackCounter = 0;
+                    moveForward = false;
                     
                 }
-                else
+                if (movedbackwards == true)
                 {
                     gameObject.transform.parent = null;
                     moveBackCounter++;
-                    if(moveBackCounter > 3)
+                   // transform.rotation = Quaternion.LookRotation(MoveDirection);
+                    Debug.Log("back");
+                    if (moveBackCounter > 3)
                     {
                         egg.SetActive(true);
-                        Debug.Log("SpawnTheEagle");
+                       
                     }
+                    movedbackwards = false;
                   
                 }
+      
+               
             }
           
         }
@@ -85,7 +106,7 @@ public class MovementScript : MonoBehaviour
 
     public IEnumerator moveTowardPosition(Transform transform)
     {
-
+        canMove = true;
         isMoving = true;
         startPosition = transform.position;
         moveTime = 0;
@@ -101,29 +122,40 @@ public class MovementScript : MonoBehaviour
 
         factor = 1f;
 
+
+       // checkForTrees();
         while (moveTime < 1f)
         {
             moveTime += Time.deltaTime * (moveSpeed / gridSize) * factor;
-
-            if(startPosition.x - endPosition.x > 0)
+            checkForTrees();
+            if (canMove)
             {
-                
-                movedbackwards = true;
+                transform.position = Vector3.Lerp(startPosition, endPosition, moveTime);
             }
-            else
-            {
-                movedbackwards = false;
+           
+            
+            
+            
                
+            
+            if (startPosition.x - endPosition.x > 0)
+            { 
+                movedbackwards = true;
+           
             }
-            transform.position = Vector3.Lerp(startPosition, endPosition, moveTime);
-            anim.SetBool("isMovingForward", false);
+            if (startPosition.x - endPosition.x < 0)
+            {
+                moveForward = true;
+            }
+          
 
             yield return null;
+
         }
 
         
        
-        Invoke("endMovement", .3f);
+        Invoke("endMovement", .2f);
        
         yield return 0;
     }
@@ -133,8 +165,38 @@ public class MovementScript : MonoBehaviour
    void endMovement()
    {
         isMoving = false;
+        
    }
 
+    void checkForTrees()
+    {
 
+
+        RaycastHit hit;
+        Vector3 direction = (endPosition - startPosition);
+        Ray ray = new Ray(startPosition, direction);
+
+        if (!Physics.Raycast(ray, out hit, direction.magnitude))
+        {
+
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+
+          
+
+        }
+        else if (hit.collider.tag == "Tree")
+        {
+
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 2, Color.blue);
+            canMove = false;
+           
+
+        }
+
+
+
+
+
+    }
 
 }
